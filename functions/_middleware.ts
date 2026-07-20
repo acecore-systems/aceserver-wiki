3d5bbcaf531818699dad6733bc46abef9bfb3783
Gui <51219838+gui-ace@users.noreply.github.com>
2026-07-20 23:03:19 +0900

    旧SurvivalRulesを現行ルールへ転送 (#13)

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -3,6 +3,11 @@ const LEGACY_ROOT_TITLES = new Set([
   'カテゴリ:メインサーバーについて',
 ])
 
+const LEGACY_ARTICLE_REDIRECTS = new Map([
+  ['/article/SurvivalRules', '/article/rule/'],
+  ['/article/SurvivalRules/', '/article/rule/'],
+])
+
 export const onRequest = async ({ request, next }) => {
   const url = new URL(request.url)
   const queryEntries = [...url.searchParams]
@@ -14,6 +19,11 @@ export const onRequest = async ({ request, next }) => {
 
   if (url.pathname === '/index.php' && isLegacyRoot) {
     return Response.redirect(`${url.origin}/`, 301)
+  }
+
+  const articleRedirect = LEGACY_ARTICLE_REDIRECTS.get(url.pathname)
+  if (articleRedirect) {
+    return Response.redirect(`${url.origin}${articleRedirect}`, 301)
   }
 
   return next()
